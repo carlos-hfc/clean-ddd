56bd135833a4a05b999d130a48f22c63515bf985
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-16 13:57:51 -0300

    feat: add fetch question comments

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from "@/core/repositories/pagination-params"
 import type { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository"
 import type { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment"
 
@@ -8,6 +9,17 @@ export class InMemoryQuestionCommentsRepository
 
   async findById(id: string): Promise<QuestionComment | null> {
     return this.items.find(item => item.id.toString() === id) ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter(item => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
