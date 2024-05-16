03e9179f968811a683eb7c77deae355c652ddb8e
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-16 12:19:22 -0300

    feat: add delete question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,11 +4,21 @@ import type { Question } from "@/domain/forum/enterprise/entities/question"
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findById(id: string): Promise<Question | null> {
+    return this.items.find(item => item.id.toString() === id) ?? null
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     return this.items.find(item => item.slug.value === slug) ?? null
   }
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
