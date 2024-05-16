78d2eed03f180d765fa7e6d341791517ba5c4a0f
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-16 12:28:39 -0300

    feat: add edit question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -12,6 +12,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return this.items.find(item => item.slug.value === slug) ?? null
   }
 
+  async save(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex(item => item.id === question.id)
+
+    this.items[itemIndex] = question
+  }
+
   async create(question: Question): Promise<void> {
     this.items.push(question)
   }
