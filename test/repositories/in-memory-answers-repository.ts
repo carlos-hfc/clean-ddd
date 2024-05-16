edaffedfbe673acdec1626209d0af82f451a9f45
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-16 12:22:35 -0300

    feat: add delete answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,17 @@ import type { Answer } from "@/domain/forum/enterprise/entities/answer"
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
+  async findById(id: string): Promise<Answer | null> {
+    return this.items.find(item => item.id.toString() === id) ?? null
+  }
+
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex(item => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
