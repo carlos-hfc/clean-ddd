21ff7084406f711fb47e0818faeddb1148edebaf
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-17 13:59:16 -0300

    feat: send notification subscriber when question best answer chosen

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,6 +4,7 @@ import { AggregateRoot } from "@/core/entities/aggregate-root"
 import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import type { Optional } from "@/core/types/optional"
 
+import { QuestionBestQuestionChosenEvent } from "../events/question-best-answer-chosen-event"
 import { QuestionAttachmentList } from "./question-attachment-list"
 import { Slug } from "./value-objects/slug"
 
@@ -75,6 +76,17 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    if (bestAnswerId === undefined) return
+
+    if (
+      this.props.bestAnswerId === undefined ||
+      !this.props.bestAnswerId.equals(bestAnswerId)
+    ) {
+      this.addDomainEvent(
+        new QuestionBestQuestionChosenEvent(this, bestAnswerId),
+      )
+    }
+
     this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
